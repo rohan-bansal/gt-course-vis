c5b97c1738363f777c6a6623b551324055abfa3d
Rohan Bansal <bansalrohan18@gmail.com>
2023-12-24 13:28:37 -0800

    title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="h-screen grid place-content-center">
+    <h1 class="text-5xl font-lemondays text-gtsecondary"><span class="text-gt">Georgia Tech</span> Course Visualizer</h1>
+    <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
+</div>
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				lemondays: ['Lemon Days']
+			},
+			colors: {
+				gt: '#EAAA00',
+				gtsecondary: '#F9F6E5'
+			}
+		}
+	},
+	plugins: []
+};
