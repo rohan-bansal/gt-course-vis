b8fbba5d567e4e745f8a42b5adb1ebea2b9c86be
Rohan Bansal <bansalrohan18@gmail.com>
2024-03-18 18:39:49 -0700

    remove switching for now

--- src/lib/data/parse.js
+++ src/lib/data/parse.js
@@ -1,11 +1,14 @@
 import semesterStore from "$lib/stores/semesterStore.js";
-import courseData from "$lib/data/202402.json";
+import courseData from "$lib/data/semesters/202402.json";
 import departments from "$lib/data/departments.json";
+// let courseData = import(`$lib/data/semesters/202402.json`);
 
 let currentSemester = "202402";
 
 semesterStore.subscribe((value) => {
   currentSemester = value;
+  console.log(currentSemester);
+  // courseData = import(`$lib/data/semesters/${currentSemester}.json`);
 });
 
 async function getCourseJSONBySemesterID(semesterID) {
--- src/routes/(withNavBar)/+layout.svelte
+++ src/routes/(withNavBar)/+layout.svelte
@@ -25,7 +25,7 @@
         <div class="grid place-content-center md:mb-0 mb-2">
             <div class="flex flex-col md:flex-row">
                 <a href="/"><h1 class="text-4xl font-lemondays text-gtsecondary mx-6"><span class="text-gt">GT</span> Co.Vis.</h1></a>
-                    {#await data.semesters}
+                    <!-- {#await data.semesters}
                         ...
                     {:then semesters} 
                         <button use:melt={$trigger} class="my-auto mx-auto text-md text-gtdark font-bold">{semesters[currentSemester]}<ChevronDown class="inline-block" /></button>
@@ -37,7 +37,7 @@
                             {/each}
                             <div use:melt={$arrow} />
                         </div>
-                    {/await}
+                    {/await} -->
             </div>
         </div>
         <div class="mx-6">
